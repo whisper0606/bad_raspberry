1c516ba9e77902c550e1e18ffe9480b4d64b4901
Will Lucas <willlucas00@gmail.com>
2026-09-22 02:38:53 +1000

    Cleaned up main, fixed curr_frame counter

--- main.c
+++ main.c
@@ -5,25 +5,24 @@
 #include "display/ssd1309_driver.h"
 
 extern const uint16_t *master[];
+uint16_t curr_frame = 0;
 bool load_next;
 
 bool timer_callback(__unused repeating_timer_t *t) {
     write_framebuf();
     load_next = true;
+    curr_frame++;
     return 0;
 }
 
 int main() {
     init_ssd1309();
-    uint16_t current_frame = 0;
     repeating_timer_t frame_timer;
-    // fire timer every 33.333ms, regardless of how long it took to run the callback.
     add_repeating_timer_us(-33333, timer_callback, NULL, &frame_timer);
-    while (current_frame <= 6752) {
+    while (curr_frame < 6752) {
         if (load_next) {
-            decompress_image(master[current_frame]);
+            decompress_image(master[curr_frame]);
             load_next = false;
         }
     }
-    
 }
