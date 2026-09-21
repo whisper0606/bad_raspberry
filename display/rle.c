f404e69f299c03408699b2af2bd5bd2369aaa8ff
Will Lucas <willlucas00@gmail.com>
2026-09-21 22:44:44 +1000

    Cleaned up rle.c/h

--- display/rle.c
+++ display/rle.c
@@ -1,16 +1,18 @@
 #include "rle.h"
 
 #define RUN_COLOR_MASK 0x8000 // clears data bits (bitwise AND)
-#define RUN_COUNT_MASK 0x7FFF  // clears color bit (bitwise AND)
+#define RUN_COUNT_MASK 0x7FFF // clears color bit (bitwise AND)
 
-void decompress_image(uint16_t img[]) {
+extern uint8_t framebuf[1024];
+
+void decompress_image(const uint16_t img[]) {
     uint8_t run = 0;
     uint16_t fb_pos = 0;
 
     while (img[run] != 0) {
         uint16_t count = img[run] & RUN_COUNT_MASK;
         uint8_t color = img[run] & RUN_COLOR_MASK;
-        
+
         for (uint8_t i = 0; i < (count >> 3); i++) {
             framebuf[fb_pos + i] = (color) ? (255) : (0);
             fb_pos++;
@@ -19,7 +21,7 @@ void decompress_image(uint16_t img[]) {
         uint8_t last_out = 0;
 
         for (uint8_t i = 0; i < (count - (count >> 3)); count++) {
-            last_out |= (1 << (7-i));
+            last_out |= (1 << (7 - i));
         }
         framebuf[fb_pos++] = last_out;
         run++;
--- display/rle.h
+++ display/rle.h
@@ -6,11 +6,7 @@
 // IMAGE FORMAT: uint16_t ARRAY.
 // RLE RUN FORMAT: uint16_t. MSB = COLOUR BIT. ALL OTHER BITS INDICATE RUN LENGTH. 
 
-extern uint8_t framebuf[1024];
-
-uint8_t prebuf[1024];
-
 /* Load an RLE compressed image into the framebuffer. */
-void decompress_image(uint16_t img[]);
+void decompress_image(const uint16_t img[]);
 
 #endif
